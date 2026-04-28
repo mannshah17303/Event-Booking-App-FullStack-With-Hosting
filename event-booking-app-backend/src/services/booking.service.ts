b621ba5e2860de26ac919c381c5e6a0af602df34
Mann Shah <mann.shah@esparkbizmail.com>
2026-04-28 17:54:01 +0530

    final event booking app with vue js as frontend and sequelize as backend

--- event-booking-app-backend/src/services/booking.service.ts
+++ event-booking-app-backend/src/services/booking.service.ts
@@ -1,22 +1,42 @@
 import { bookingRepository } from "../repositories/booking.repository";
 
 export const bookingService = {
-  
-  async addBooking(data: {user_id:string, event_id:number, booking_date:string, quantity:number, price_per_ticket:number, paymentIntentId:string}) {
+  async addBooking(data: {
+    user_id: string;
+    event_id: number;
+    booking_date: string;
+    quantity: number;
+    price_per_ticket: number;
+    paymentIntentId: string;
+  }) {
     const booking = await bookingRepository.addBooking(data);
     return booking;
   },
-  async createPaymentIntent(data: {user_id:string, event_id:number, booking_date:string, quantity:number, price_per_ticket:number}) {
+  async createPaymentIntent(data: {
+    user_id: string;
+    event_id: number;
+    booking_date: string;
+    quantity: number;
+    price_per_ticket: number;
+  }) {
     const booking = await bookingRepository.createPaymentIntent(data);
     return booking;
   },
   async getAllBookingDetailsForPieChart() {
     return await bookingRepository.getAllBookingDetailsForPieChart();
   },
-  async updateBookingRatings(currentUserId:string, ratingId:number, ratingStar:number){
-    return await bookingRepository.updateBookingRatings(currentUserId, ratingId, ratingStar)
+  async updateBookingRatings(
+    currentUserId: string,
+    ratingId: number,
+    ratingStar: number,
+  ) {
+    return await bookingRepository.updateBookingRatings(
+      currentUserId,
+      ratingId,
+      ratingStar,
+    );
   },
-  async deleteBooking(id:string){
+  async deleteBooking(id: any) {
     return await bookingRepository.deleteBooking(id);
-  }
+  },
 };
